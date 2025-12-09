a3bb2e925b47ad25d73682b23480e08c0519e21c
Freyja <freyja@freyja-lynx.dev>
2025-12-09 12:41:04 -0800

    part 1 exercises 3,4,5

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -10,7 +10,7 @@ const Part = (props) => {
   return (
     <>
       <p>
-        {props.part} {props.exercises}
+        {props.name} {props.exercises}
       </p>
     </>
   )
@@ -20,7 +20,7 @@ const Content = (props) => {
   return (
     <div>
       {props.parts.map((partx) => (
-        <Part key={partx.part} part={partx.part} exercises={partx.exercises} />
+        <Part key={partx.name} name={partx.name} exercises={partx.exercises} />
       ))}
     </div>
   )
@@ -35,25 +35,29 @@ const Total = (props) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-
-  const content = [
-    {part: part1, exercises: exercises1},
-    {part: part2, exercises: exercises2},
-    {part: part3, exercises: exercises3}
-  ];
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={content} />
-      <Total subtotals={[exercises1, exercises2, exercises3]} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total subtotals={course.parts.map((x) => x.exercises)} />
     </div>
   )
 }
