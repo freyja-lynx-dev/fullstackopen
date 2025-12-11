013ab835340e825c5307d795d16fa1ebb4535f58
Freyja <freyja@freyja-lynx.dev>
2025-12-10 18:30:15 -0800

    exercise 2.6

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,6 +1,8 @@
 import Course from './components/Course'
+import Note from './components/Note'
+import { useState } from 'react'
 
-const App = () => {
+const CoursesApp = () => {
   const courses = [
     {
       name: 'Half Stack application development',
@@ -51,6 +53,96 @@ const App = () => {
       {courses.map((course) => {
         return <Course course={course} />
       })}
+    </div>
+  )
+}
+
+const NotesApp = (props) => {
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState('enter a note here!')
+  const [showAll, setShowAll] = useState(true)
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+      id: String(notes.length + 1)
+    }
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
+  }
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
+
+  const notesToShow = showAll ? notes : notes.filter(note => note.important)
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>show {showAll ? 'important' : 'all'}</button>
+      </div>
+      <ul>
+        {notesToShow.map(note => 
+          <Note key={note.id} note={note} />
+        )}
+      </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange}/>
+        <button type="submit">save</button>
+      </form>
+    </div>
+  )
+}
+
+const Person = ({name}) => (<>{name}</>)
+
+const Persons = ({persons}) => (
+  <ul>
+    {persons.map((x) =>
+      <li key={x.name}>
+        <Person name={x.name} />
+      </li>
+    )}
+  </ul>
+)
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const addEntry = (event) => {
+    event.preventDefault()
+    const entryObject = {
+      name: newName,
+    }
+    setPersons(persons.concat([entryObject]))
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Persons persons={persons} />
+      <form onSubmit={addEntry}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      ...
     </div>
   )
 }
