b6c2db202fdfacf2863efc4517d5378acd84c12a
Freyja <freyja@freyja-lynx.dev>
2025-12-16 10:38:37 -0800

    part 2 exercise 11

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,6 +1,7 @@
 import Course from './components/Course'
 import Note from './components/Note'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const CoursesApp = () => {
   const courses = [
@@ -154,12 +155,14 @@ const PersonEntryForm = ({callback}) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456'},
-    { name: 'Ada Lovelace', number: '39-44-5323523'},
-    { name: 'Dan Abramov', number: '12-43-234345'},
-    { name: 'Mary Poppendieck', number: '39-23-6423122'}
-  ]) 
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  },[])
+  const [persons, setPersons] = useState([]) 
 
   const addEntry = ({newName, newNumber}) => {
     const entryObject = {
--- part2/src/main.jsx
+++ part2/src/main.jsx
@@ -1,24 +1,6 @@
 import App from './App.jsx'
 import ReactDOM from 'react-dom/client'
 
-const notes = [
-  {
-    id: 1,
-    content: 'html is easy peasy',
-    important: true
-  },
-  {
-    id: 2,
-    content: 'browser can only execute javascript (and wasm)',
-    important: false
-  },
-  {
-    id: 3,
-    content: 'GET and POST are the most important http protocol methods!!',
-    important: true
-  }
-]
-
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App notes={notes} />
+  <App />
 )
